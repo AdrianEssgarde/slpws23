b567dc32c329d302422ae156fb3a3fd948b101c0
AdrianEssgarde <95085134+AdrianEssgarde@users.noreply.github.com>
2023-02-09 11:44:07 +0100

    Delete + item och description visas
    Jag har fått både items och descrpitions att visas på sidan där alla annonser visas. Jag har även lyckats få delete funktionen att fungera och ska nästa gång försöka få resternade crud funktioner att fungera.

--- app.rb
+++ app.rb
@@ -13,9 +13,11 @@ end
 get("/items/") do
     db = SQLite3::Database.new("db/shop.db")
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM item")
-    p "All items from result: #{result}"
-    slim(:"/index", locals:{item:result})
+    result1 = db.execute("SELECT * FROM item")
+    result2 = db.execute("SELECT * FROM description")
+    p "All items from result: #{result1}"
+    p "All items from result: #{result2}"
+    slim(:"/index", locals:{item:result1, description:result2})
 end
 
 get("/item/new") do
@@ -34,6 +36,10 @@ post("/item/new") do
 end
 
 post("/item/:id/delete") do
-    id = params[:id].to
-
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/shop.db")
+    db.execute("DELETE From item WHERE id = ?", id)
+    db.execute("DELETE From description WHERE id = ?", id)
+    redirect("/items/")
 end
+
